6ca9b75a65a26d85ab03beefc1973e072239e3ff
Lê Minh Qúy <125531081+quyk67uet@users.noreply.github.com>
2025-06-06 21:44:32 +0700

    Update next.config.js

--- elearning/www/nextjs-app/next.config.js
+++ elearning/www/nextjs-app/next.config.js
@@ -2,10 +2,30 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, // THÊM DÒNG NÀY ĐỂ TẮT TỐI ƯU HÓA HÌNH ẢNH
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http", // Thêm cho learn.local nếu bạn phục vụ ảnh từ Frappe qua Nginx
+        hostname: "math.local",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      },
+    ],
   },
-  trailingSlash: true,
-  output: "export",
 };
 
 module.exports = nextConfig;
